e1036457e153c89b2c199a54b79844bfbb06df19
PaulQz <mn1bvc1@mail.ru>
2019-11-21 19:23:45 +0200

    feat: add solution

--- itechart lab/script.js
+++ itechart lab/script.js
@@ -4,4 +4,43 @@ $('#exampleModal').on('show.bs.modal', function (event) {
     var modal = $(this)
     modal.find('.modal-title').text('New message to ' + recipient)
     modal.find('.modal-body input').val(recipient)
-  })
+  }) 
+
+  var users = {
+    name: 'ne Sava',
+    lastname: 'ne Down',
+    username: 'ne @sad'
+  };
+
+
+  function loadUsers() {
+    
+  }
+
+  function screenUsers() {
+
+    var usersTable = document.getElementById('users-table');
+    var data = usersTable.getElementsByTagName('td');
+
+    for(let i=0; i < data.length; i++) {
+      data[i].onclick = function() {
+        if(data[i].cellIndex==1) {
+          console.log(data[i].innerText = users.name);
+          console.log(data[i+1].innerText = users.lastname);
+          console.log(data[i+2].innerText = users.username); //:)
+        }
+      }
+    }
+  }
+
+  function enterAdmin() {
+    var login = document.getElementById('login');
+    var password = document.getElementById('password');
+    var adnimForm = document.getElementById('adminEnter');
+    if(login.innerHTML == 'admin' && password.innerHTML == 'admin') {
+      adnimForm.action = 'admin profile/index.html';
+    }
+    else alert('nope');
+  }
+
+  
